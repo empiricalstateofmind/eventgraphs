85149f75a8a29ca5a14097b8f291dbe47ac71170
Andy Mellor <mellor91@hotmail.co.uk>
2018-01-24 11:08:05 +0000

    Added basic read/write tests

--- tests/test_eventgraph.py
+++ tests/test_eventgraph.py
@@ -3,24 +3,51 @@ from unittest import TestCase
 
 import env
 import data
-from eventgraphs import EventGraph
+from eventgraphs import EventGraph, BadInputError
 
 class InputOutputTests(TestCase):
 	"""
 	Tests the input and output functionality of EventGraph class.
 	"""
 
+	# Run only if pandas is installed?
 	def test_from_pandas(self):
-		pass
+		""""""
+		import pandas as pd # Really want to remove the pandas dependency
+		df = pd.DataFrame(data.directed)
+		EG = EventGraph.from_pandas_eventlist(events=df,
+											  graph_rules='teg')
+		self.assertTrue(True)
 
 	def test_from_dict(self):
-		pass
+		""""""
+		EG = EventGraph.from_dict_eventlist(events=data.directed,
+									  		graph_rules='teg')
+		self.assertTrue(True)
 
-	def test_from_file(self):
-		pass
+
+	def test_from_save_and_load(self):
+		""""""
+
+		EG = EventGraph.from_dict_eventlist(events=data.directed,
+									  		graph_rules='teg')
+		EG._build()
+		EG.save('test.json')
+		LG = EventGraph.from_file('test.json')
+		# Cannot compare dataframes where columns are in different order
+		LG.eg_edges = LG.eg_edges[EG.eg_edges.columns] 
+
+		self.assertTrue((LG.events == EG.events).all().all())
+		self.assertTrue((LG.eg_edges == EG.eg_edges).all().all())
+		self.assertTrue((LG.events_meta == EG.events_meta).all().all())
+		self.assertTrue(hasattr(LG, 'event_pair_processed'))
 
 	def test_bad_input(self):
-		pass
+		""""""
+		with self.assertRaises(BadInputError):
+			EG = EventGraph(events=['abc','cba'],
+							graph_rules='teg')
+		
 
 
 class BuildTests(TestCase):
@@ -48,12 +75,12 @@ class ComponentsTests(TestCase):
 	"""
 
 	def setUp(self):
-		self.EG = EventGraph.from_dict_eventlist(events=data.directed_hyper,
+		self.EG = EventGraph.from_dict_eventlist(events=data.string_labels,
 											graph_rules='teg')
 		self.EG._build()
 
 	def tearDown(self):
-		pass
+		self.EG = None
 
 	def test_connected_components(self):
 		components = self.EG.connected_components()
