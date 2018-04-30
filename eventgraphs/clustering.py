a831616348f5cfc6c044e54752e299672bc642aa
Andy Mellor <mellor91@hotmail.co.uk>
2018-04-30 14:44:36 +0100

    Added basic clustering and clustering assignment for new samples

--- eventgraphs/clustering.py
+++ eventgraphs/clustering.py
@@ -1,6 +1,9 @@
 import pandas as pd
-
 from collections import defaultdict
+
+from scipy.spatial.distance import pdist
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 from .analysis import (calculate_motif_distribution, 
 					   calculate_motif_entropy,
@@ -116,3 +119,25 @@ def generate_features(components, feature_spec=FEATURE_SPEC, verbose=False):
 	features = pd.DataFrame(features).T.fillna(0)
 	scale_features = pd.DataFrame(scale_features).T.fillna(0)
 	return features, scale_features
+
+def similarity(features, metric='euclidean', normalize=True):
+    """ Normalise features and calculate similarity matrix """
+    if normalize:
+        X = StandardScaler().fit_transform(features.values)
+    else:
+        X = features.values
+    sim = pdist(X, metric)
+    return sim
+
+def find_clusters(similarity, kind='ward', criterion='maxclust', max_clusters=4):
+    """"""
+    Z = linkage(similarity, kind)
+    clusters = fcluster(Z, max_clusters, criterion='maxclust')
+    
+    cluster_centers = pd.DataFrame({cluster:features.loc[clusters==cluster].mean() for cluster in range(1, max_clusters+1)})
+    return clusters, cluster_centers
+
+def assign_to_cluster(observation, cluster_centers):
+	""" Assign an observation to a cluster. """
+
+	return (cluster_centers.subtract(observation, axis=0)**2).sum().idxmin()
