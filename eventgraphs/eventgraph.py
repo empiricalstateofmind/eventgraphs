8e21b6b2201a3990fe722f7dba7568fec27b1caf
Andy Mellor <mellor91@hotmail.co.uk>
2018-08-22 10:58:09 +0100

    Check for int now includes numpy types

--- eventgraphs/eventgraph.py
+++ eventgraphs/eventgraph.py
@@ -523,7 +523,7 @@ class EventGraph(object):
                 attrs = {'type': event.type, 'color': edge_colormap[event.type]} if typed  else {'color':'black'}
                 if isinstance(event.target, Iterable) and (len(event.target) == 0):
                     G.add_node(event.source)
-                elif isinstance(event.target, str) or isinstance(event.target, int):
+                elif isinstance(event.target, str) or isinstance(event.target, np.integer):
                     G.add_edge(event.source, event.target, **attrs)
                 else:
                     for target in event.target:
