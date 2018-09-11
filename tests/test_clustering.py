79cbbc70720060a40dbf4ce9b056d3b45aa97820
Andy Mellor <mellor91@hotmail.co.uk>
2018-09-11 09:32:10 +0100

    Tests now correctly using space indents

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -11,30 +11,30 @@ import numpy as np
 from pandas.testing import assert_frame_equal
 
 class CompletitionTests(TestCase):
-	"""
-	Tests that each function runs (not currently testing output).
-	"""
+    """
+    Tests that each function runs (not currently testing output).
+    """
 
-	def setUp(self):
-		self.EG = EventGraph.from_file("test.json")
-		self.components = self.EG.connected_components()
+    def setUp(self):
+        self.EG = EventGraph.from_file("test.json")
+        self.components = self.EG.connected_components()
 
-	def tearDown(self):
-		self.EG = None
+    def tearDown(self):
+        self.EG = None
 
-	@unittest.skip("Skipping until we have motifs in the prebuild EGs")
-	def test_generate_features(self):
-		""""""
-		features, scale_features = generate_features(self.components, 
-													 feature_spec=FEATURE_SPEC)
+    @unittest.skip("Skipping until we have motifs in the prebuild EGs")
+    def test_generate_features(self):
+        """"""
+        features, scale_features = generate_features(self.components, 
+                                                     feature_spec=FEATURE_SPEC)
 
-	def test_generate_distance_matrix(self):
-		""""""
-		A = pd.DataFrame(np.random.random(size=(20,100)))
-		result = generate_distance_matrix(A, metric='euclidean')
+    def test_generate_distance_matrix(self):
+        """"""
+        A = pd.DataFrame(np.random.random(size=(20,100)))
+        result = generate_distance_matrix(A, metric='euclidean')
 
-	def test_generate_linkage(self):
-		""""""
-		A = pd.DataFrame(np.random.random(size=(20,100)))
-		distance = generate_distance_matrix(A, metric='euclidean')
-		result = generate_linkage(distance, kind='ward')
+    def test_generate_linkage(self):
+        """"""
+        A = pd.DataFrame(np.random.random(size=(20,100)))
+        distance = generate_distance_matrix(A, metric='euclidean')
+        result = generate_linkage(distance, kind='ward')
--- tests/test_eventgraph.py
+++ tests/test_eventgraph.py
@@ -9,103 +9,103 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 DATASETS = [data.directed, 
-			data.directed_hyper, 
-			data.directed_hyper_single, 
-			data.undirected_hyper,
-			data.extra_columns,
-			data.string_labels]
+            data.directed_hyper, 
+            data.directed_hyper_single, 
+            data.undirected_hyper,
+            data.extra_columns,
+            data.string_labels]
 
 class IOTests(TestCase):
-	"""
-	Tests the input and output functionality of EventGraph class.
-	"""
+    """
+    Tests the input and output functionality of EventGraph class.
+    """
 
-	def from_pandas(self, dataset):
-		""""""
-		df = pd.DataFrame(dataset)
-		EG = EventGraph.from_pandas_eventlist(events=df,
-											  graph_rules='teg')
-		self.assertTrue(True)
+    def from_pandas(self, dataset):
+        """"""
+        df = pd.DataFrame(dataset)
+        EG = EventGraph.from_pandas_eventlist(events=df,
+                                              graph_rules='teg')
+        self.assertTrue(True)
 
-	def from_dict(self, dataset):
-		""""""
-		EG = EventGraph.from_dict_eventlist(events=dataset,
-									  		graph_rules='teg')
-		self.assertTrue(True)
+    def from_dict(self, dataset):
+        """"""
+        EG = EventGraph.from_dict_eventlist(events=dataset,
+                                              graph_rules='teg')
+        self.assertTrue(True)
 
-	def from_save_and_load(self, dataset):
-		""""""
+    def from_save_and_load(self, dataset):
+        """"""
 
-		EG = EventGraph.from_dict_eventlist(events=dataset,
-									  		graph_rules='teg')
-		EG.build()
-		EG.save('test.json')
-		LG = EventGraph.from_file('test.json')
-		# Cannot compare dataframes where columns are in different order
-		LG.eg_edges = LG.eg_edges[EG.eg_edges.columns] 
+        EG = EventGraph.from_dict_eventlist(events=dataset,
+                                              graph_rules='teg')
+        EG.build()
+        EG.save('test.json')
+        LG = EventGraph.from_file('test.json')
+        # Cannot compare dataframes where columns are in different order
+        LG.eg_edges = LG.eg_edges[EG.eg_edges.columns] 
 
-		assert_frame_equal(LG.events, EG.events)
-		assert_frame_equal(LG.eg_edges, EG.eg_edges)
-		assert_frame_equal(LG.events_meta, EG.events_meta)
-		self.assertTrue(hasattr(LG, '_event_pair_processed'))
+        assert_frame_equal(LG.events, EG.events)
+        assert_frame_equal(LG.eg_edges, EG.eg_edges)
+        assert_frame_equal(LG.events_meta, EG.events_meta)
+        self.assertTrue(hasattr(LG, '_event_pair_processed'))
 
-	def test_from_dict(self):
-		""" """
-		for dataset in DATASETS:
-			self.from_dict(dataset)
+    def test_from_dict(self):
+        """ """
+        for dataset in DATASETS:
+            self.from_dict(dataset)
 
-	def test_from_pandas(self):
-		""" """
-		for dataset in DATASETS:
-			self.from_pandas(dataset)
+    def test_from_pandas(self):
+        """ """
+        for dataset in DATASETS:
+            self.from_pandas(dataset)
 
-	def test_from_save_and_load(self):
-		""" """
-		for dataset in DATASETS:
-			self.from_save_and_load(dataset)
+    def test_from_save_and_load(self):
+        """ """
+        for dataset in DATASETS:
+            self.from_save_and_load(dataset)
 
-	def test_bad_input(self):
-		""""""
-		with self.assertRaises(BadInputError):
-			EG = EventGraph(events=['abc','cba'],
-							graph_rules='teg')
-		
+    def test_bad_input(self):
+        """"""
+        with self.assertRaises(BadInputError):
+            EG = EventGraph(events=['abc','cba'],
+                            graph_rules='teg')
+        
 
 
 class BuildTests(TestCase):
-	"""
-	"""
-	pass
+    """
+    """
+    pass
 
 
 class MatrixMethodTests(TestCase):
-	"""
-	Tests the methods associated with building matrices from data.
-	"""
-	pass
+    """
+    Tests the methods associated with building matrices from data.
+    """
+    pass
 
 
 class FilterTests(TestCase):
-	"""
-	"""
-	pass
+    """
+    """
+    pass
 
 
 class ComponentsTests(TestCase):
-	"""
-	Tests functionality of component decomposition.
-	"""
+    """
+    Tests functionality of component decomposition.
+    """
 
-	def setUp(self):
-		self.EG = EventGraph.from_dict_eventlist(events=data.string_labels,
-											graph_rules='teg')
-		self.EG.build()
+    def setUp(self):
+        self.EG = EventGraph.from_dict_eventlist(events=data.string_labels,
+                                            graph_rules='teg')
+        self.EG.build()
 
-	def tearDown(self):
-		self.EG = None
+    def tearDown(self):
+        self.EG = None
 
-	def test_connected_components(self):
-		components = self.EG.connected_components()
-		
+    def test_connected_components(self):
+        components = self.EG.connected_components()
+        
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
